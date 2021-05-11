d07b325250f42602942dbbf03f3d1f469b82b5bd
Michal Vyšinský <michal@vysinsky.cz>
2021-05-11 21:03:25 +0200

    feat: add example scripts

---
+++ Editor/ExampleEditorScript.cs
@@ -0,0 +1,17 @@
+﻿using cz.vysinsky.unity.examplepackage.runtime;
+using UnityEditor;
+
+namespace cz.vysinsky.unity.examplepackage.editor
+{
+    [CustomEditor(typeof(ExampleBehaviour))]
+    class ExampleEditorScript: Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            ExampleBehaviour myTarget = (ExampleBehaviour)target;
+
+            myTarget.ExampleProperty = EditorGUILayout.IntField("Example Property", myTarget.ExampleProperty);
+            EditorGUILayout.LabelField("Example Property", myTarget.ExampleProperty.ToString());
+        }
+    }
+}
---
+++ Runtime/ExampleBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace cz.vysinsky.unity.examplepackage.runtime
+{
+    public class ExampleBehaviour : MonoBehaviour
+    {
+
+       public int ExampleProperty;
+
+        private void Start()
+        {
+            Debug.Log("Log from ExampleBehaviour");
+        }
+    }
+}
